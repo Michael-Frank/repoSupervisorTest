1aed4da9420b648a46cd1834e9830728b409ce56
Michael-Frank <michael.frank@qaware.de>
2018-11-29 18:15:07 +0100

    update

---
+++ secrets.java
@@ -0,0 +1,30 @@
+public class secrets {
+    private static final String someother = "config";
+    private static final String FOO = "no-secret";
+    private static final String FOO_BAR = "q28Wt3nAmLt_3NGpqi2qz-jQ7";
+    private static final String d7kyociU24P9hJ_sYVkqzo-kE ="foobar";
+    private static final String pw15 = "SnyXN4r9RsLrK7yq";
+    private static final String api-key ="${api-key-form-env}";
+    private static final String api-key2 ="65c05687159dff4b04236ae5d5aae8be45419703";
+    private static final String secret40 = "2fb386d44a64a7abd600b8d03863935ccd7b875f2c0449cb37c7982b35b8306a1f71d19595be9b6a";
+    private static final String secret60 = "80d7cc4997454e2c12986900afd0a5b650a6c9b3463a0bef777f269372614e9b9e462d45d63a2ecb214285a704255fd9a82c534e22e677bf52ee5496";
+    private static final String secret120 = "deb7ada4dafed229177f6a5437091e51e0845067c0e6da885e1f3cf42e9b6e95851b19098656f88a2f1c46face69878c76eec4eedde398d38960f06b6bc11fcdfa89137b93195f499d1cf799d7f016acf55607d2a8fa16c93afd8dac893cb27e6d6b34a45ef882fea52c4b375eaff34f4fdd4190567857531469f48ab7be89cae815d81a089fb102b5b774fa8a1f10629df88ccddcb21b3c1ba78c2fd72fb0c4";
+    private static final String secret120_multi =//
+                    "aeb7ada4dafed229177f6a5437091e51e0845067" +//
+                    "c0e6da885e1f3cf42e9b6e95851b19098656f88a" +//
+                    "2f1c46face69878c76eec4eedde398d38960f06b" +//
+                    "6bc11fcdfa89137b93195f499d1cf799d7f016ac" +//
+                    "f55607d2a8fa16c93afd8dac893cb27e6d6b34a4" +//
+                    "5ef882fea52c4b375eaff34f4fdd419056785753" +//
+                    "1469f48ab7be89cae815d81a089fb102b5b774fa" +//
+                    "8a1f10629df88ccddcb21b3c1ba78c2fd72fb0c4";//
+
+    private static final String passphrase = "unwoven ravishing cabdriver lubricate providing sneeze steerable thinness fringe clothing ruby reprogram mourner thrash wow";
+    private static final Object some = new Object() {
+        private static final Object nested = new Object() {
+            private static final String secret = "90d7cc4997454e2c12986900afd0a5b650a6c9b3463a0bef777f269372614e9b9e462d45d63a2ecb214285a704255fd9a82c534e22e677bf52ee5496";
+
+        }
+    }
+
+}
